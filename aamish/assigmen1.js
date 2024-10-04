3bb00c92fbdd0e2513774db5d46f15970ad44e6f
anaomar72 <anabomer541@gmail.com>
2024-10-04 13:55:46 +0100

    tijaabintilabaad

---
+++ aamish/assigmen1.js
@@ -0,0 +1,9 @@
+function subtraction() {
+    // subtraction
+ let number1=number(prompt("soo geli mushaarkda"))
+let number2=number(prompt(" soo geli inta aad bishaan mushaar ahaan u heshay "))
+let NUM3=number1-number2
+document.write("mushaarkda waa  " ," =  ", "$", num3,"dollar" )
+
+
+}
